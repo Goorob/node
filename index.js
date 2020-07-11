070590f7687eab2aa27e563e5e7e1b6348e86140
unknown <Goorob>
2020-07-11 17:58:14 +0300

    rest without express nodejs

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) =>{
 })
         
     }
+    if(req.url == '/api/users'){
+        const users = [
+            {name : 'sunset' , age :25 },
+            {name : 'Demon' , age : 120}
+        ]
+        res.writeHead(200 , {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(users));
+                
+            }
 });
 const PORT = process.env.PORT || 5000 ;
 server.listen(PORT , () => console.log(`server running on port ${PORT} and you're not`));
