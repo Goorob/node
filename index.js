5f0d57b986f9308b4eb68f63cbcee80dad19a755
unknown <Goorob>
2020-07-11 17:41:15 +0300

    http

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const Person = require('./person.js');
-const person1 = new Person('john ' , 25);
-person1.greeting();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const server = http.createServer((req , res) =>{
+    if(req.url == '/'){
+        res.writeHead(200 , {'Content-Type': 'text/html'});
+        res.end('<h1> RUN </h1>')
+    }
+});
+const PORT = process.env.PORT || 5000 ;
+server.listen(PORT , () => console.log(`server running on port ${PORT} and you're not`));
